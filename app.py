92b402071c2d516091f9c0648e7014b1fbde8efa
brandjamie8 <brandjamie8@gmail.com>
2025-02-13 21:05:50 +0000

    Update app.py

--- app.py
+++ app.py
@@ -310,7 +310,7 @@ if 'df' not in st.session_state:
                 "PathwayCount": over52
             })
     #df_dummy = pd.DataFrame(data_list)
-    df_dummy = pd.read_csv("PTT_PTL_23_24.csv")
+    df_dummy = pd.read_csv("RTT_PTL_23_24.csv")
     st.session_state.df = df_dummy.copy()
 
 df = st.session_state.df.copy()
